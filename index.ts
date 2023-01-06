2a2c0a372cf2d8064da960281ca929ae5e5f9e48
Jamal Soueidan <jamal@soueidan.com>
2023-01-06 17:03:46 +0300

    add docker and herou

--- index.ts
+++ index.ts
@@ -4,16 +4,34 @@ import assetsRouter from "./assets-router";
 
 const { PORT = 8000 } = process.env;
 
+const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
+const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
+
 export async function createServer(
-  root = process.cwd()
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === "production"
 ) {
   const app = express();
+
+  if (isProd) {
+    app.use("/", express.static(PROD_INDEX_PATH));
+  } else {
+    app.use("/", assetsRouter);
+  }
 
   app.get("/api/v1", (req, res) => {
     res.json({
       project: "React and Express Boilerplate",
       from: "Vanaldito",
     });
+  });
+
+  app.get("/*", (_req, res) => {
+    const htmlFile = path.join(
+      isProd ? PROD_INDEX_PATH + "index.html" : DEV_INDEX_PATH + "dev.html"
+    );
+
+    res.sendFile(htmlFile);
   });
 
   return { app };
