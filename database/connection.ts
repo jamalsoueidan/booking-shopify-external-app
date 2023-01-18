3ed9aa3720a33a39340250c6c8dcc5d5f0e834bc
Jamal Soueidan <jamal@soueidan.com>
2023-01-18 11:31:33 +0300

    add schema after connection

--- database/connection.ts
+++ database/connection.ts
@@ -1,8 +1,35 @@
+import {
+  BookingSchema,
+  CartSchema,
+  CollectionSchema,
+  CustomerSchema,
+  NotificationSchema,
+  NotificationTemplateSchema,
+  ProductSchema,
+  ScheduleSchema,
+  SettingSchema,
+  ShopifySessionSchema,
+  StaffSchema,
+  UserSchema,
+} from "@jamalsoueidan/bsb.bsb-pkg";
 import mongoose from "mongoose";
 
 export const connection = async () => {
   try {
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.model("Booking", BookingSchema);
+    mongoose.model("Cart", CartSchema);
+    mongoose.model("Collection", CollectionSchema);
+    mongoose.model("Customer", CustomerSchema);
+    mongoose.model("NotificationTemplate", NotificationTemplateSchema);
+    mongoose.model("Notification", NotificationSchema);
+    mongoose.model("Product", ProductSchema);
+    mongoose.model("Schedule", ScheduleSchema);
+    mongoose.model("Setting", SettingSchema);
+    mongoose.model("ShopifySessions", ShopifySessionSchema);
+    mongoose.model("Staff", StaffSchema);
+    mongoose.model("User", UserSchema);
     console.log("Connecting to MongoDB Atlas cluster...");
   } catch (error) {
     console.error("Connection to MongoDB Atlas failed!", error);
