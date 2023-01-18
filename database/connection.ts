d50a35c61377df88a7a72be94ac39cbadf6d81e9
Jamal Soueidan <jamal@soueidan.com>
2023-01-18 11:49:14 +0300

    update msg for connection

--- database/connection.ts
+++ database/connection.ts
@@ -8,7 +8,7 @@ import {
   ProductSchema,
   ScheduleSchema,
   SettingSchema,
-  ShopifySessionSchema,
+  ShopifySessionsSchema,
   StaffSchema,
   UserSchema,
 } from "@jamalsoueidan/bsb.bsb-pkg";
@@ -16,21 +16,23 @@ import mongoose from "mongoose";
 
 export const connection = async () => {
   try {
+    console.log("Trying connecting to MongoDB...");
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGODB_URI);
-    mongoose.model("Booking", BookingSchema);
-    mongoose.model("Cart", CartSchema);
-    mongoose.model("Collection", CollectionSchema);
-    mongoose.model("Customer", CustomerSchema);
-    mongoose.model("NotificationTemplate", NotificationTemplateSchema);
-    mongoose.model("Notification", NotificationSchema);
-    mongoose.model("Product", ProductSchema);
-    mongoose.model("Schedule", ScheduleSchema);
-    mongoose.model("Setting", SettingSchema);
-    mongoose.model("ShopifySessions", ShopifySessionSchema);
-    mongoose.model("Staff", StaffSchema);
-    mongoose.model("User", UserSchema);
-    console.log("Connecting to MongoDB Atlas cluster...");
+    mongoose.connect(process.env.MONGODB_URI, () => {
+      mongoose.model("Booking", BookingSchema);
+      mongoose.model("Cart", CartSchema);
+      mongoose.model("Collection", CollectionSchema);
+      mongoose.model("Customer", CustomerSchema);
+      mongoose.model("NotificationTemplate", NotificationTemplateSchema);
+      mongoose.model("Notification", NotificationSchema);
+      mongoose.model("Product", ProductSchema);
+      mongoose.model("Schedule", ScheduleSchema);
+      mongoose.model("Setting", SettingSchema);
+      mongoose.model("ShopifySessions", ShopifySessionsSchema);
+      mongoose.model("Staff", StaffSchema);
+      mongoose.model("User", UserSchema);
+      console.log("MongoDB is Connected");
+    });
   } catch (error) {
     console.error("Connection to MongoDB Atlas failed!", error);
   }
