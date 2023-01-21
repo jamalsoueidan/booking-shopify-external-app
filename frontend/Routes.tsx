e2f4294b626e7524687b36adb829d938dd3800bd
Jamal Soueidan <jamal@soueidan.com>
2023-01-21 03:59:04 +0300

    lazy load all pages

--- frontend/Routes.tsx
+++ frontend/Routes.tsx
@@ -1,48 +1,43 @@
 import { LoadingPage } from "@jamalsoueidan/bsf.bsf-pkg";
-import BookingNew from "pages/booking/booking.new";
-import Schedules from "pages/schedules";
-import Setting from "pages/setting";
-import Staff from "pages/staff";
 import NotFound from "pages/NotFound";
-import Dashboard from "pages/dashboard";
 import { ProtectedRoute } from "providers/Protected";
 import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Auth from "pages/auth/auth";
-import Login from "@pages/auth/login";
-import Phone from "@pages/auth/phone";
 
+const Dashboard = lazy(() => import("pages/dashboard"));
 const Booking = lazy(() => import("pages/booking"));
+const Schedules = lazy(() => import("pages/schedules"));
+const BookingNew = lazy(() => import("pages/booking/booking.new"));
+const Setting = lazy(() => import("pages/setting"));
+const Staff = lazy(() => import("pages/staff"));
+const Login = lazy(() => import("pages/auth/login"));
+const Phone = lazy(() => import("pages/auth/phone"));
+const Auth = lazy(() => import("pages/auth/auth"));
 
 export default () => {
   return (
     <Routes>
-      <Route
-        path="dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      >
+      <Suspense fallback={<LoadingPage title="Loading bookings" />}>
         <Route
-          path="bookings"
+          path="dashboard"
           element={
-            <Suspense fallback={<LoadingPage title="Loading bookings" />}>
-              <Booking />
-            </Suspense>
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
           }
-        />
-        <Route path="bookings/new" element={<BookingNew />} />
-        <Route path="schedules" element={<Schedules />} />
-        <Route path="staff" element={<Staff />} />
-        <Route path="settings" element={<Setting />} />
-      </Route>
-      <Route path="/" element={<Auth />}>
-        <Route path="login" element={<Login />} />
-        <Route path="phone" element={<Phone />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
+        >
+          <Route path="bookings" element={<Booking />} />
+          <Route path="bookings/new" element={<BookingNew />} />
+          <Route path="schedules" element={<Schedules />} />
+          <Route path="staff" element={<Staff />} />
+          <Route path="settings" element={<Setting />} />
+        </Route>
+        <Route path="/" element={<Auth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="phone" element={<Phone />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Suspense>
     </Routes>
   );
 };
